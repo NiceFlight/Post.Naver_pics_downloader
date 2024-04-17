6095fd93b55ba616af24e8f2dc5043a8f37157ef
NiceFlight <167335404+NiceFlight@users.noreply.github.com>
2024-04-18 02:32:45 +0800

    Add files via upload

---
+++ Post.Naver_pics_downloader.py
@@ -0,0 +1,64 @@
+import os
+import time
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# 用 selenium 建立目標網站連線
+driver = webdriver.Chrome()
+url = input(f"Enter link address:")
+# url = "https://post.naver.com/viewer/postView.naver?volumeNo=37653713"
+driver.get(url)
+# 待網頁載入
+time.sleep(3)
+
+# 用 beautifulsoup 解析
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+# print(soup)
+
+# 建立資料夾
+print(dir)
+dir_name = f"images/{url.split("=")[-1]}"
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+    print(f"file created successed")
+else:
+    print(f"file existed")
+
+# 找目標網址的標籤
+imgs = soup.find_all("img")
+allow_file_name = ["jpg", "png", "jpeg", "gif"]
+# print(len(imgs))
+for img in imgs:
+    # 找目標網址
+    src = img.get("src")
+    # print(src)
+    if not src:
+        continue
+
+    # 尋找並建立圖片檔案名稱
+    filename = src.split("/")[-1].split(".")[0]
+    # print(filename)
+    extension = src.split(".")[-1][0:3]
+    # print(extension)
+    if extension in allow_file_name:
+        # print(src)
+        print(f"file type：{extension}")
+        print(f"url:{src}")
+
+        # 建立下載連線
+        headers = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/125.0.0.0 Safari/537.36"
+        }
+        # .split("?")[0]
+        download = requests.get(src.split("?")[0], headers=headers, stream=True)
+        if download.status_code == 200:
+            # print(f"YES")
+            # 儲存圖片
+            with open(f"images/{url.split("=")[-1]}/{filename}.{extension}", 'wb') as file:
+                file.write(download.content)
+            print(f"{filename} downloading{"." * 20}\n")
+            # time.sleep(3)
+driver.close()
